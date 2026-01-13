16e8b22a13d8208424cb33854ac4a1019cb891a1
chloeejp <hoiteenj@gmail.com>
2026-01-13 22:52:20 +0700

    Update lsp.lua

--- nvim/lua/rim/plugins/lsp.lua
+++ nvim/lua/rim/plugins/lsp.lua
@@ -17,6 +17,9 @@ return {
         },
     },
     config = function()
+        local capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        require("lspconfig").lua_ls.setup { capabilities = capabilities }
+
         vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
         vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
         vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
